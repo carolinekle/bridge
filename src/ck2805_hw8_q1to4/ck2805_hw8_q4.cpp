df4e58fde48a7a5d2cfa68675c73b28675dc2794
carolinekle <caroline.klewinowski@gmail.com>
2025-03-07 20:59:33 -0500

    complete and submit

--- src/ck2805_hw8_q1to4/ck2805_hw8_q4.cpp
+++ src/ck2805_hw8_q1to4/ck2805_hw8_q4.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int jumpIt(){
+int jumpIt(int arr[], int index, int size){
+    if(index==size-1){
+        return arr[index];
+    }else{
+        if (index + 2 < size){
+            if(arr[index+1]<arr[index+2]){
+                return arr[index]+jumpIt(arr, index+1, size);
     
+            }else{
+                return arr[index]+ jumpIt(arr, index+2, size);
+            }
+        }
+       
+    }
+
 }
 
 int main(){
+
+    int index=0;
+    int size=6;
+    int arr[6] = {0, 3, 80, 6, 57, 10};
+
+    int cost = jumpIt(arr, index, size);
+
+    cout<<cost<<endl;
 
     return 0;
 }
