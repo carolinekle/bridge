54d5f729c79a89bf1265ac8ce12efc3da0c06636
carolinekle <caroline.klewinowski@gmail.com>
2025-03-22 21:19:08 -0400

    hw

--- src/ck2805_hw9/ck2805_hw9.cpp
+++ src/ck2805_hw9/ck2805_hw9.cpp
@@ -1,26 +1,91 @@
 #include <iostream>
 using namespace std;
 
-class Organism{
+// sim must take vector of ants and doodlebugs ?
+class Sim{
+    private:
+        int time;
+        // array with world 
+        char world [400];
+        
+
+    public:
+        Sim();
+        void fillWorld();// fill world with default '-' and bugs
+        void displayWorld();// create sim
+        char getWorld();
+        char setWorld();
+
+        ~Sim();
+
+};
+//figure out how to pass/get present version of world without fill world 
+/*
+void Sim::getWorld(){
+
+    return 
+}
+*/
+
+/* class Bug{
+    private:
+
     public:
         int move();
         int breed();
+        int timeSurvived();
+        void update(); //update time and movement of organisms  
+
+};
+
+class Ant:public Bug{
+    private:
+    public:
+    // must overide breed/time and move
+    //getter and setter for bug location?
+};
+
+class Doodlebug:public Bug{
     private:
 
-};
-
-class Ant:public Organism{
-
-};
-
-class Doodlebug:public Organism{
     public:
         int starve();
-    private:
+        int eat();
+        // must overide breed/time and move
+        //getter and setter for bug location?
+}; */
 
-};
+
+
+
+void Sim::fillWorld(){
+    
+
+}
+
+void Sim::displayWorld(){
+    //create sim board 
+    //must use generated bug data here. would render empty world
+
+    cout << "World at time: "<<time<<endl;
+    cout<<endl;
+    for(int i =0; i <20; i++){
+        for(int j=0; j<20; j++){
+            cout<<world[i] << " ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    cout<< "Press ENTER to continue."<<endl;
+
+}
 
 int main(){
+
+    Sim Sim;
+
+    Sim.fillWorld();
+    Sim.displayWorld();
 
 
     return 0;
